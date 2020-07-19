7cd550750cc092bfc8d1b3e73c73c0e80b5b3af1
degath <zaleskid1@gmail.com>
2020-07-19 20:48:45 +0200

    initial commit

---
+++ application/src/main/java/pl/degath/application/RecruitmentTaskApp.java
@@ -0,0 +1,13 @@
+package pl.degath.application;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTaskApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentTaskApp.class, args);
+    }
+}
